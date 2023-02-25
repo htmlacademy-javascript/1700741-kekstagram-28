a7af65a6f559c89572f87b6467a24f97907006f0
alenachesnokova <chesnokovaeg@mail.ru>
2023-02-25 20:40:26 +0500

    2.28. Нужно больше функций после консультации

--- js/functions.js
+++ js/functions.js
@@ -1,74 +1,129 @@
 /**
- *
- * @param {string} string
- * @param {number} checkLength
+ * Проверит, подходит ли строка по длине.
+ * @param {string} target
+ * @param {number} length
+ * @return {boolean}
  */
+const fitsLength = (target, length) => target.length <= length;
 
-const checkLengthString = (string, checkLength) => (string.length <= checkLength);
-
-checkLengthString('hello', 20);
-checkLengthString('hello', 4);
+fitsLength('проверяемая строка', 10);
 
 /**
- *
- * @param {string} string
+ * Проверит, является ли строка (или число) палиндромом.
+ * @param {string|number} target
+ * @return {boolean}
+ * @see https://en.wikipedia.org/wiki/Palindrome
  */
+const isPalindrome = (target) => {
+  const normalized = String(target).replaceAll(' ', '').toLowerCase();
+  const reversed = normalized.split('').reverse().join('');
 
-const checkPalindrome = (string) => {
-  const symbols = string.toLowerCase().split('');
-
-  const letters = symbols.filter((symbol) => (symbol !== ' '));
-
-  for (let i = 0; i <= letters.length / 2; i++) {
-
-    if (letters[i] !== letters[(letters.length - 1 - i)]) {
-      return false;
-    }
-  }
-  return true;
+  return normalized === reversed;
 };
-checkPalindrome('Hello mama');
+
+isPalindrome('Лёша на полке клопа нашёл ');
 
 /**
- *
- * @param {*} string
+ * Извлечёт из строки (или числа) цифры от 0 до 9.
+ * @param {string|number} target
+ * @return {number}
  */
-const extractNumbers = (string) => {
+const parseDigits = (target) => {
+  const digits = String(target).replace(/[^0-9]/g, '');
 
-  const stringConverted = (typeof(string) === 'number') ? String(string) : string ;
-
-  const arrayNumbers = [];
-
-  for (let i = 0; i < stringConverted.length; i++) {
-    if (!isNaN(parseInt(stringConverted[i], 10))) {
-      arrayNumbers.push(stringConverted[i]);
-    }
-  }
-
-  const finishNumber = Number(arrayNumbers.join(''));
-  return (finishNumber === 0) ? NaN : finishNumber;
+  return digits ? Number(digits) : NaN;
 };
 
-extractNumbers('hello154');
+parseDigits('1 кефир, 0.5 батона');
 
+/**
+ * Добавит в начало строки символы другой строки.
+ * В результате исходная строка достигнет заданной длины.
+ * @param {string} target
+ * @param {number} length
+ * @param {string} pad
+ * @return {string}
+ */
+const padStart = (target, length, pad) => {
+  const start = pad.repeat(length);
 
-const createAddressFiles = (string, minLength, addSymbol) => {
-
-  let addressFile = '';
-  let finishAddSymbol = '';
-
-  if (addSymbol.length + string.length >= minLength) {
-    addressFile = `${addSymbol.substr(0, (minLength - string.length))}${string}`;
-    return addressFile;
-  }
-
-  for (let i = 0; i < (minLength - addSymbol.length - string.length); i++) {
-    finishAddSymbol += addSymbol[i];
-  }
-
-  addressFile = finishAddSymbol + addSymbol + string;
-
-  return addressFile;
+  return `${start}${target}`.slice(-length);
 };
 
-createAddressFiles('aa', 4, 'bbb');
+padStart('1', 4, '0');
+
+
+// /**
+//  *
+//  * @param {string} string
+//  * @param {number} checkLength
+//  */
+
+// const checkLengthString = (string, checkLength) => (string.length <= checkLength);
+
+// checkLengthString('hello', 20);
+// checkLengthString('hello', 4);
+
+// /**
+//  *
+//  * @param {string} string
+//  */
+
+// const checkPalindrome = (string) => {
+//   const symbols = string.toLowerCase().split('');
+
+//   const letters = symbols.filter((symbol) => (symbol !== ' '));
+
+//   for (let i = 0; i <= letters.length / 2; i++) {
+
+//     if (letters[i] !== letters[(letters.length - 1 - i)]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// };
+// checkPalindrome('Hello mama');
+
+// /**
+//  *
+//  * @param {*} string
+//  */
+// const extractNumbers = (string) => {
+
+//   const stringConverted = (typeof(string) === 'number') ? String(string) : string ;
+
+//   const arrayNumbers = [];
+
+//   for (let i = 0; i < stringConverted.length; i++) {
+//     if (!isNaN(parseInt(stringConverted[i], 10))) {
+//       arrayNumbers.push(stringConverted[i]);
+//     }
+//   }
+
+//   const finishNumber = Number(arrayNumbers.join(''));
+//   return (finishNumber === 0) ? NaN : finishNumber;
+// };
+
+// extractNumbers('hello154');
+
+
+// const createAddressFiles = (string, minLength, addSymbol) => {
+
+//   let addressFile = '';
+//   let finishAddSymbol = '';
+
+//   if (addSymbol.length + string.length >= minLength) {
+//     addressFile = `${addSymbol.substr(0, (minLength - string.length))}${string}`;
+//     return addressFile;
+//   }
+
+//   for (let i = 0; i < (minLength - addSymbol.length - string.length); i++) {
+//     finishAddSymbol += addSymbol[i];
+//   }
+
+//   addressFile = finishAddSymbol + addSymbol + string;
+
+//   return addressFile;
+// };
+
+// createAddressFiles('aa', 4, 'bbb');
